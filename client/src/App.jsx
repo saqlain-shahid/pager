497d67d6684f33030f8884676eab1231238f8acf
SAQLAIN SHAHID <saqlain.shahid03@gmail.com>
2024-03-08 23:55:41 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,38 @@
+import React, {lazy} from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import ProtectRoute from './components/auth/ProtectRoute'
+
+
+const Home = lazy(() => import ( './pages/Home'))
+const Chat = lazy(() => import ( './pages/Chat'))
+const Groups = lazy(() => import ( './pages/Groups'))
+const Login = lazy(() => import ( './pages/Login'))
+const NotFound = lazy(() => import ( './pages/NotFound'))
+
+let user = true
+
+const App = () => {
+  return (
+   <BrowserRouter>
+      <Routes>
+         <Route element={<ProtectRoute user={user}/>}>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/chat/:chatId' element={<Chat/>}/>
+            <Route path='/groups' element={<Groups/>}/>
+         </Route>
+
+         <Route 
+         path='/login' 
+         element={
+         <ProtectRoute user={!user}>
+            <Login/>
+         </ProtectRoute>}/>
+
+
+         <Route path='*' element={<NotFound/>}/>
+      </Routes>
+   </BrowserRouter>
+  )
+}
+
+export default App
