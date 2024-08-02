425ca6d3a60e1e87e784e874ec2541547bb89199
SAQLAIN SHAHID <saqlain.shahid03@gmail.com>
2024-08-03 03:04:39 +0530

    added validatorss and checks

--- server/routes/chat.router.js
+++ server/routes/chat.router.js
@@ -3,21 +3,25 @@ import express from 'express'
 import { isAuthenticated } from '../middlewares/auth.js'
 import { addMembers, deleteChat, getChatDetails, getMessages, getMyChats, getMyGroups, leaveGroup, newGroupChat, removeMember, renameGroup, sendAttachments } from '../controllers/chat.controller.js'
 import { attachmentsMulter } from '../middlewares/multer.js'
+import { addMemberValidator, chatIdValidator, newGroupValidator, removeMemberValidator, renameValidator, sendAttachmentsValidator, validateHandler } from '../lib/validators.js'
 
 
 const router = express.Router()
  
 //user loggedin routes
 router.use(isAuthenticated)
-router.post('/new',newGroupChat)
+router.post('/new',newGroupValidator(), validateHandler, newGroupChat)
 router.get('/my',getMyChats)
 router.get('/my/groups',getMyGroups)
-router.put('/addmembers',addMembers)
-router.put('/removemember',removeMember)
-router.delete('/leave/:id',leaveGroup)
-router.post('/message', attachmentsMulter, sendAttachments)
-router.get('/message/:id', getMessages)
-router.route('/:id').get(getChatDetails).put(renameGroup).delete(deleteChat)
+router.put('/addmembers',addMemberValidator(), validateHandler, addMembers)
+router.put('/removemember',removeMemberValidator(), validateHandler, removeMember)
+router.delete('/leave/:id',chatIdValidator(), validateHandler, leaveGroup)
+router.post('/message', attachmentsMulter, sendAttachmentsValidator(), validateHandler, sendAttachments)
+router.get('/message/:id',chatIdValidator(), validateHandler, getMessages)
+router.route('/:id')
+.get(chatIdValidator(), validateHandler, getChatDetails)
+.put(renameValidator(), validateHandler, renameGroup)
+.delete(chatIdValidator(), validateHandler, deleteChat)
 
 
 export default router
--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -1,18 +1,34 @@
-import express from 'express'
-import { getMyProfile, login, logout, newUser, searchUser } from '../controllers/user.controller.js'
-import { singleAvatar } from '../middlewares/multer.js'
-import { isAuthenticated } from '../middlewares/auth.js'
+import express from "express";
+import {
+  getMyProfile,
+  login,
+  logout,
+  newUser,
+  searchUser,
+} from "../controllers/user.controller.js";
+import { singleAvatar } from "../middlewares/multer.js";
+import { isAuthenticated } from "../middlewares/auth.js";
+import {
+  loginValidator,
+  registerValidator,
+  validateHandler,
+} from "../lib/validators.js";
 
+const router = express.Router();
 
-const router = express.Router()
+router.post(
+  "/new",
+  singleAvatar,
+  registerValidator(),
+  validateHandler,
+  newUser
+);
+router.post("/login", loginValidator(), validateHandler, login);
 
-router.post('/new', singleAvatar, newUser)
-router.post('/login',login)
- 
 //user loggedin routes
-router.use(isAuthenticated)
-router.get('/profile',getMyProfile)
-router.get('/logout',logout)
-router.get('/search',searchUser)
+router.use(isAuthenticated);
+router.get("/profile", getMyProfile);
+router.get("/logout", logout);
+router.get("/search", searchUser);
 
-export default router
+export default router;
