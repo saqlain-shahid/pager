b486d7e6c18243436b6df1928a3a8afc84a8ea91
SAQLAIN SHAHID <saqlain.shahid03@gmail.com>
2024-08-04 02:21:43 +0530

    made admin api

---
+++ server/controllers/admin.controller.js
@@ -0,0 +1,147 @@
+import { Chat } from "../models/chat.model.js";
+import { Message } from "../models/message.model.js";
+import { User } from "../models/user.model.js";
+import { TryCatch } from "../utils/features.js";
+// import jwt from "jsonwebtoken";
+// import { ErrorHandler } from "../utils/utility.js";
+// import { cookieOptions } from "../utils/features.js";
+// import { adminSecretKey } from "../app.js";
+
+const allUsers = TryCatch(async (req, res) => {
+  const users = await User.find({});
+
+  const transformedUsers = await Promise.all(
+    users.map(async ({ name, username, avatar, _id }) => {
+      const [groups, friends] = await Promise.all([
+        Chat.countDocuments({ groupChat: true, members: _id }),
+        Chat.countDocuments({ groupChat: false, members: _id }),
+      ]);
+
+      return {
+        name,
+        username,
+        avatar: avatar.url,
+        _id,
+        groups,
+        friends,
+      };
+    })
+  );
+
+  return res.status(200).json({
+    status: "success",
+    users: transformedUsers,
+  });
+});
+
+// allchatss
+const allChats = TryCatch(async (req, res) => {
+  const chats = await Chat.find({})
+    .populate("members", "name avatar")
+    .populate("creator", "name avatar");
+
+  const transformedChats = await Promise.all(
+    chats.map(async ({ members, _id, groupChat, name, creator }) => {
+      const totalMessages = await Message.countDocuments({ chat: _id });
+
+      return {
+        _id,
+        groupChat,
+        name,
+        avatar: members.slice(0, 3).map((member) => member.avatar.url),
+        members: members.map(({ _id, name, avatar }) => ({
+          _id,
+          name,
+          avatar: avatar.url,
+        })),
+        creator: {
+          name: creator?.name || "None",
+          avatar: creator?.avatar.url || "",
+        },
+        totalMembers: members.length,
+        totalMessages,
+      };
+    })
+  );
+
+  return res.status(200).json({
+    status: "success",
+    chats: transformedChats,
+  });
+});
+
+//all messages
+const allMessages = TryCatch(async (req, res) => {
+  const messages = await Message.find({})
+    .populate("sender", "name avatar")
+    .populate("chat", "groupChat");
+
+  const transformedMessages = messages.map(
+    ({ content, attachments, _id, sender, createdAt, chat }) => ({
+      _id,
+      attachments,
+      content,
+      createdAt,
+      chat: chat._id,
+      groupChat: chat.groupChat,
+      sender: {
+        _id: sender._id,
+        name: sender.name,
+        avatar: sender.avatar.url,
+      },
+    })
+  );
+
+  return res.status(200).json({
+    success: true,
+    messages: transformedMessages,
+  });
+});
+
+//dashboardd
+const getDashboardStats = TryCatch(async (req, res) => {
+  const [groupsCount, usersCount, messagesCount, totalChatsCount] =
+    await Promise.all([
+      Chat.countDocuments({ groupChat: true }),
+      User.countDocuments(),
+      Message.countDocuments(),
+      Chat.countDocuments(),
+    ]);
+
+  const today = new Date();
+
+  const last7Days = new Date();
+  last7Days.setDate(last7Days.getDate() - 7);
+
+  const last7DaysMessages = await Message.find({
+    createdAt: {
+      $gte: last7Days,
+      $lte: today,
+    },
+  }).select("createdAt");
+
+  const messages = new Array(7).fill(0);
+  const dayInMiliseconds = 1000 * 60 * 60 * 24;
+
+  last7DaysMessages.forEach((message) => {
+    const indexApprox =
+      (today.getTime() - message.createdAt.getTime()) / dayInMiliseconds;
+    const index = Math.floor(indexApprox);
+
+    messages[6 - index]++;
+  });
+
+  const stats = {
+    groupsCount,
+    usersCount,
+    messagesCount,
+    totalChatsCount,
+    messagesChart: messages,
+  };
+
+  return res.status(200).json({
+    success: true,
+    stats,
+  });
+});
+export { allUsers, allChats, allMessages,getDashboardStats };
--- server/routes/admin.router.js
+++ server/routes/admin.router.js
@@ -1,12 +1,15 @@
 import express from 'express'
+import { allChats, allMessages, allUsers, getDashboardStats } from '../controllers/admin.controller.js'
 
 const router = express.Router()
 
 router.get('/')
 router.post('/verify')
 router.get('/logout')
-router.get('/users')
-router.get('/chats')
-router.get('/messages')
+
+router.get('/users', allUsers)
+router.get('/chats', allChats)
+router.get('/messages',allMessages)
+router.get('/stats',getDashboardStats)
 
 export default router
